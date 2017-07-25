97f05d44a7cdcde74e4dd9c0d76a5d6454b61b3a
Peter Hanley <phanley@temple.edu>
2017-07-25 14:01:05 -0600

    update offline assets (vue.js only for now)

---
+++ template/offline_assets/update_assets.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl --request "GET" "https://unpkg.com/vue" --out vue.js
+
+### add something to update to latest bootstrap/foundation/jquery as needed
+# curl --request "GET" "https://code.jquery.com/jquery-3.2.1.js" --out jquery.js
